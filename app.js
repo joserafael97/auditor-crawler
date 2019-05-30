f908d486e45a163cb9b78003b8d76f59d71e4038
JoseRafael97 <jose.rafael.feitosa@gmail.com>
2019-05-29 23:37:03 -0300

    correções

--- app.js
+++ app.js
@@ -18,55 +18,52 @@ import Node from './bfs/node';
 
 connectToDb(); 
 
+const element = new Element('http://portaldatransparencia.publicsoft.com.br/sistemas/ContabilidadePublica/views/views_control/index.php?cidade=O5w=&uf=PB',
+    null, null, null, null)
 
-// const element = new Element('http://portaldatransparencia.publicsoft.com.br/sistemas/ContabilidadePublica/views/views_control/index.php?cidade=O5w=&uf=PB',
-//     null, null, null, null)
-
-// let root = new Node(element, [], [], false);
+let root = new Node(element, [], [], false);
 
 
-// const logErrorAndExit = err => {
-//     console.log(err);
-//     process.exit();
-// };
-
-// let evaluation = Evaluation({
-//     date: new Date(),
-//     county: 'Ouro Velho',
-//     cityHallUrl: 'http://ourovelho.pb.gov.br',
-//     transparencyPortalUrl: element.getValue(),
-// });
-
-// let initDate = new Date();
-
-// let criterionDespesaOrc = CrawlerUtil.createCriterion('Despesa Orçamentária');
-// let criterionDespesaExtra = CrawlerUtil.createCriterion('Despesa Extra Orçamentária');
-// let criterionReceitaOrc = CrawlerUtil.createCriterion('Receita Orçamentária');
-// let criterionReceitaExtra = CrawlerUtil.createCriterion('Receita Extra Orçamentária');
-// let criterionLicit = CrawlerUtil.createCriterion('Licitação');
-// let criterionPessoal = CrawlerUtil.createCriterion('Quadro Pessoal');
-
-const run = async () => {
-    // connectToDb();
-    // criterion = await Criterion.addCriterion(criterion, await Bfs.bfsInit(root, null, [], criterion, evaluation, []).catch(logErrorAndExit));
-    // evaluation.dateEnd = new Date();
-    // evaluation.duration = evaluation.dateEnd.getTime() - initDate.getTime()
-    // const delta = Math.abs(new Date() - initDate) / 1000;
-    // const minutes = Math.floor(delta / 60) % 60;
-    // evaluation.durationMin = minutes;
-    // await Evaluation.addEvaluationWithOneCriterion(evaluation, criterion)
-  
-    // console.log(criterion);
-    // console.log("============================================================================");
-    // console.log("Duration: ", minutes, ' min')
-    console.log((await Evaluation.findLastByCounty('Ouro Velho')));
+const logErrorAndExit = err => {
+    console.log(err);
+    process.exit();
 };
 
-run();
+let evaluation = Evaluation({
+    date: new Date(),
+    county: 'Ouro Velho',
+    cityHallUrl: 'http://ourovelho.pb.gov.br',
+    transparencyPortalUrl: element.getValue(),
+});
 
-// run(criterionDespesaOrc);
-// run(criterionDespesaExtra);
-// run(criterionReceitaExtra);
-// run(criterionReceitaOrc);
-// run(criterionLicit);
-// run(criterionPessoal);
+let initDate = new Date();
+
+let criterionDespesaOrc = CrawlerUtil.createCriterion('Despesa Orçamentária');
+let criterionDespesaExtra = CrawlerUtil.createCriterion('Despesa Extra Orçamentária');
+let criterionReceitaOrc = CrawlerUtil.createCriterion('Receita Orçamentária');
+let criterionReceitaExtra = CrawlerUtil.createCriterion('Receita Extra Orçamentária');
+let criterionLicit = CrawlerUtil.createCriterion('Licitação');
+let criterionPessoal = CrawlerUtil.createCriterion('Quadro Pessoal');
+
+const run = async (criterion) => {
+    connectToDb();
+    criterion = await Criterion.addCriterion(criterion, await Bfs.bfsInit(root, null, [], criterion, evaluation, []).catch(logErrorAndExit));
+    evaluation.dateEnd = new Date();
+    evaluation.duration = evaluation.dateEnd.getTime() - initDate.getTime()
+    const delta = Math.abs(new Date() - initDate) / 1000;
+    const minutes = Math.floor(delta / 60) % 60;
+    evaluation.durationMin = minutes;
+    await Evaluation.addEvaluationWithOneCriterion(evaluation, criterion)
+  
+    console.log(criterion);
+    console.log("============================================================================");
+    console.log("Duration: ", minutes, ' min')
+
+};
+
+run(criterionDespesaOrc);
+run(criterionDespesaExtra);
+run(criterionReceitaExtra);
+run(criterionReceitaOrc);
+run(criterionLicit);
+run(criterionPessoal);
