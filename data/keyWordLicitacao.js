f4d59c21058d6cc3ef456ad75e6b03dae4a7da16
JoseRafael97 <jose.rafael.feitosa@gmail.com>
2019-06-28 17:54:19 -0300

    correções

--- data/keyWordLicitacao.js
+++ data/keyWordLicitacao.js
@@ -6,12 +6,12 @@ const identificationKeyLicitacao = {
     "numero_licitacao": ["numero da licitacao", "nº licitacao", "numero", "processo",
         "numero", "N° Processo"
     ],
-    "objeto_licitacao": ["Objeto", "descricao do objeto", "objeto resumido",
+    "objeto_licitacao": ["Objeto", "Objeto da Licitação", "descricao do objeto", "objeto resumido",
         "tipo do objeto"
     ],
     "modalidade_licitacao": ["tipo", "modalidade", "categoria"],
     "data_publicacao_licitacao": ["data publicacao", "data", "data da homogacao",
-        "data da homologacao",
+        "data da homologacao", "Publicação",
         "data da publicacao", "dt. homologacao", "homologacao"
     ],
     "valor": ["valor", 'VALOR R$'],
@@ -23,7 +23,7 @@ const identificationKeyLicitacao = {
     "setor_interessado": ["setor interessado", "reparticao/setor interessado:", "orgao"],
     "integra": ["integra"],
     "termo_ratificacao": ["ratificacao", ],
-    "pregao": ["pregao", "pregao eletronico", "tipo", "modalidade", "categoria"],
+    "pregao": ["pregao", "pregao eletronico"],
     "aviso": ["edital", "publicacao", "aviso"],
     "licitado": ["licitado", "licitacao"]
 }
