db267ab3486a84ae2f40060abc537e7d6e6db657
joserafael97 <jose.rafael.feitosa@gmail.com>
2019-11-03 22:06:24 +0000

    addes function to data proccess

--- banditProcessClassifier.js
+++ banditProcessClassifier.js
@@ -14,7 +14,6 @@ import FeaturesConst from './consts/featuares';
 export default class BanditProcessClassifier {
 
     static async initilize(node, puppeteer = null, queue, criterion, evaluation, elementsAccessed = [], itens = null, model, epsilonGreedyAlg, xTrain = [], yTrain = [], actuallyIndex = 0, contNodeNumber = 1, trainModel = []) {
-
         if (puppeteer == null) {
             puppeteer = await PuppeteerUtil.createPuppetterInstance();
         }
@@ -48,10 +47,12 @@ export default class BanditProcessClassifier {
         if (queue.length > 0 && CrawlerUtil.checkItensComplete(itens) === false) {
 
             epsilonGreedyAlg.updateNumArms(queue.length);
-            node.updateRewardNodes();
+            //node.updateRewardNodes();
 
-            if (node.getLevel() > 1 && lengthQueueBefore < queue.length) {
+            if (node.getLevel() > 1) {
                 for (let i = 0; i < queue.length; i++) {
+                    xPred = this.formatData(this.getFeaturesWithOutResultNode(queue[i]));
+                    console.log("---------xPred--------------", xPred)
                     const maxReward = queue[i].getMaxReward();
                     if (maxReward > 0)
                         epsilonGreedyAlg.update(i, maxReward)
@@ -117,6 +118,57 @@ export default class BanditProcessClassifier {
         newData["result"] = (node.getFeatures()[FeaturesConst.MORE_ITEM_CRITERIO] === 1 || node.getFeatures()[FeaturesConst.ONE_ITEM_CRITERIO] === 1) ? 'identification_item' : node.getEdges().length > 0 ? 'component_relevant' : 'no_relevant';
         return newData;
     }
+
+    static getFeaturesWithOutResultNode(node) {
+        let newData = {};
+        newData[FeaturesConst.URL_RELEVANT] = node.getFeatures()[FeaturesConst.URL_RELEVANT];
+        newData[FeaturesConst.MORE_THAN_ONE_NEW_COMPONENT_PARENT] = node.getParent().getFeatures()[FeaturesConst.MORE_THAN_ONE_NEW_COMPONENT];
+        newData[FeaturesConst.URL_RELEVANT_PARENT] = node.getParent().getFeatures()[FeaturesConst.URL_RELEVANT];
+        newData[FeaturesConst.TERM_CRITERION_PARENT] = node.getParent().getFeatures()[FeaturesConst.TERM_CRITERION];
+        newData[FeaturesConst.ONE_ITEM_CRITERIO_PARENT] = node.getParent().getFeatures()[FeaturesConst.ONE_ITEM_CRITERIO];
+        newData[FeaturesConst.MORE_ITEM_CRITERIO_PARENT] = node.getParent().getFeatures()[FeaturesConst.MORE_ITEM_CRITERIO];
+
+        let urlRelevantBrother = 0;
+        let moreThanOneNewComponentBrother = 0;
+        let oneItemCriterionBrother = 0;
+        let moreItemCriterionBrother = 0;
+        let termCriterionBrother = 0;
+
+        for (const brotherNode of node.getParent().getChildrenResearchedNodes()) {
+            urlRelevantBrother = brotherNode.getFeatures()[FeaturesConst.MORE_ITEM_CRITERIO] + urlRelevantBrother;
+            moreThanOneNewComponentBrother = brotherNode.getFeatures()[FeaturesConst.MORE_THAN_ONE_NEW_COMPONENT] + moreThanOneNewComponentBrother;
+            oneItemCriterionBrother = brotherNode.getFeatures()[FeaturesConst.ONE_ITEM_CRITERIO] + oneItemCriterionBrother;
+            moreItemCriterionBrother = brotherNode.getFeatures()[FeaturesConst.MORE_ITEM_CRITERIO] + moreItemCriterionBrother;
+            termCriterionBrother = brotherNode.getFeatures()[FeaturesConst.TERM_CRITERION] + termCriterionBrother;
+
+        }
+
+        newData[FeaturesConst.URL_RELEVANT_BRORHER] = urlRelevantBrother;
+        newData[FeaturesConst.MORE_THAN_ONE_NEW_COMPONENT_BRORHER] = moreThanOneNewComponentBrother;
+        newData[FeaturesConst.ONE_ITEM_CRITERIO_BRORHER] = oneItemCriterionBrother;
+        newData[FeaturesConst.MORE_ITEM_CRITERIO_BRORHER] = moreItemCriterionBrother;
+        newData[FeaturesConst.TERM_CRITERION_BRORHER] = termCriterionBrother;
+
+        return newData;
+    }
+
+    static formatData(data) {
+        return [
+            data[FeaturesConst.URL_RELEVANT],
+            data[FeaturesConst.MORE_THAN_ONE_NEW_COMPONENT_PARENT],
+            data[FeaturesConst.URL_RELEVANT_PARENT],
+            data[FeaturesConst.TERM_CRITERION_PARENT],
+            data[FeaturesConst.ONE_ITEM_CRITERIO_PARENT],
+            data[FeaturesConst.MORE_ITEM_CRITERIO_PARENT],
+            data[FeaturesConst.URL_RELEVANT_BRORHER],
+            data[FeaturesConst.MORE_THAN_ONE_NEW_COMPONENT_BRORHER],
+            data[FeaturesConst.ONE_ITEM_CRITERIO_BRORHER],
+            data[FeaturesConst.MORE_ITEM_CRITERIO_BRORHER],
+            data[FeaturesConst.TERM_CRITERION_BRORHER],
+        ];
+    }
+
+
 
 
 
