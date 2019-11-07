813e7820f1c3f7af1a3c47d662bdd6de9b85b38b
joserafael97 <jose.rafael.feitosa@gmail.com>
2019-11-07 15:30:01 -0300

    corretions in time sleep in node proccess

--- utils/xpathUtil.js
+++ utils/xpathUtil.js
@@ -7,7 +7,7 @@ import { QUERYTODYNAMICELEMENT, QUERYTOSTATICCOMPONENT } from '../models/queryEl
 const EQUALTYPESEARCH = 'tagSearch = "{}"';
 const CONTAINSTYPESEARCH = 'contains(tagSearch, "{}")';
 const XPATHIFRAME = '//*/iframe';
-const UNUSABLEIFRAMES = ['limpo', 'youtube', 'blank', 'google', 'sharethis','graficos', 'Graficos', 'DXR.axd', "javascript", 
+const UNUSABLEIFRAMES = ['limpo', 'youtube', 'tweet', 'blank', 'google', 'sharethis','graficos', 'Graficos', 'DXR.axd', "javascript", 
 'assets','anexo', 'download', 'widget', ".zip", ".jpeg", ".rar", "noticia", "publicidade",'facebook', "email", 'whatsapp', 'print', 'png', 'dist'];
 const CONSULTAR = "consultar";
 const PESQUISAR = "pesquisar";
