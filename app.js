c75fb3dd6d2e0c47cf03e4d007e09efa5fcf3f72
joserafael97 <jose.rafael.feitosa@gmail.com>
2019-11-19 13:57:31 -0300

    corretions in runing in et-cons

--- app.js
+++ app.js
@@ -208,16 +208,16 @@ function sleep(ms) {
 
     Promise.all([
         startCrawler(evaluation, criterionDespesaOrc),
-        // await sleep(1000),
-        // startCrawler(evaluation, criterionDespesaExtra),
-        // await sleep(1000),
-        // startCrawler(evaluation, criterionReceitaOrc),
-        // await sleep(1000),
-        // startCrawler(evaluation, criterionReceitaExtra),
-        // await sleep(1000),
-        // startCrawler(evaluation, criterionLicit),
-        // await sleep(1000),
-        // startCrawler(evaluation, criterionPessoal)
+        await sleep(1000),
+        startCrawler(evaluation, criterionDespesaExtra),
+        await sleep(1000),
+        startCrawler(evaluation, criterionReceitaOrc),
+        await sleep(1000),
+        startCrawler(evaluation, criterionReceitaExtra),
+        await sleep(1000),
+        startCrawler(evaluation, criterionLicit),
+        await sleep(1000),
+        startCrawler(evaluation, criterionPessoal)
     ]).then((result) => {
         console.log("=================finished=======================");
         moogoseInstace.connection.close(function () {
