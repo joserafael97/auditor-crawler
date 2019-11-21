8d1539f08248a258e77bbb466f9705b3f91e9ee7
joserafael97 <jose.rafael.feitosa@gmail.com>
2019-11-21 17:58:53 -0300

    update shell script

--- runAllcounties.sh
+++ runAllcounties.sh
@@ -2,4 +2,4 @@
 
 aproach="dfs"
 
-cat counties.txt | xargs -P 2  -d '\n' -l1 -I value nohup npm start county=value aproach="$aproach" allitens="true" > runLogs/"$i"_"$aproach"_"$(date +%d_%m_%Y_%H_%M_%S_%N)".out.txt &
+cat counties.txt | xargs -P 2  -d '\n' -l1 -I value nohup npm start county=value aproach="$aproach" allitens="true" > runLogs/"$value"_"$aproach"_"$(date +%d_%m_%Y_%H_%M_%S_%N)".out.txt 2>&1 &
