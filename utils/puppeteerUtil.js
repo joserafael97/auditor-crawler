e7a72e960bb42fad2ec4e4f375272aef4b6c8f00
joserafael97 <jose.rafael.feitosa@gmail.com>
2019-11-27 18:05:24 -0300

    correçoes

--- utils/puppeteerUtil.js
+++ utils/puppeteerUtil.js
@@ -93,23 +93,30 @@ export default class PuppeteerUtil {
     static async accessParent(page, parents) {
         if (parents.length > 0) {
             const nodeParent = parents[0];
+
             if (HtmlUtil.isUrl(nodeParent.getSource().getValue())) {
-                Promise.all([page.goto(nodeParent.getSource().getValue()).catch(e => void e), page.waitForNavigation().catch(e => void e)]);
+                Promise.all([page.goto(nodeParent.getSource().getValue()).catch(e => void e), page.waitForNavigation({ timeout: 3000 }).catch(e => void e)]);
+
             } else {
                 const currentPage = page;
                 for (let parent of parents.reverse()) {
                     let source = parent.getSource();
+
                     if (HtmlUtil.isUrl(source.getValue())) {
-                        Promise.all([page.goto(source.getValue()).catch(e => void e), page.waitForNavigation().catch(e => void e)]);
+                        Promise.all([page.goto(source.getValue()).catch(e => void e), page.waitForNavigation({ timeout: 3000 }).catch(e => void e)]);
+
                     } else {
+
                         if (parent.getSource().getIsExtractIframe() && (await page.constructor.name) !== "Frame") {
-                            await page.waitForNavigation().catch(e => void e);
+                            await page.waitForNavigation({ timeout: 3000 }).catch(e => void e);
                             page = await PuppeteerUtil.detectContext(page).catch(e => void e);
                         }
+
                         let element = await PuppeteerUtil.selectElementPage(page, source.getXpath(), source.getValue());
+
                         if (element) {
                             await element.click().catch(e => void e);
-                            await page.waitForNavigation().catch(e => void e);
+                            await page.waitForNavigation({ timeout: 3000 }).catch(e => void e);
                         }
                     }
                 }
@@ -121,21 +128,25 @@ export default class PuppeteerUtil {
 
     static async selectElementPage(page, xpath, searchValue) {
 
-        await page.waitForNavigation().catch(e => void e);
-        await page.waitFor(6000).catch(e => void e);
-        let elements = []
-        elements = await page.$x(xpath).catch(e => void e);
-        
-        if (elements !== undefined && elements.length > 0) {
-            for (let element of elements) {
-                let text = await (await element.getProperty('textContent')).jsonValue();
-                const propertyHandleValue = await (await element.getProperty('value')).jsonValue();
-                text = HtmlUtil.isUrl(text) ? text : TextUtil.normalizeText(TextUtil.removeWhiteSpace(text));
-                text = text != undefined && text.length > 0 ? text :
-                    propertyHandleValue != undefined && propertyHandleValue.length > 0 ?
-                        TextUtil.normalizeText(TextUtil.removeWhiteSpace(propertyHandleValue)) : '';
-                if (text === searchValue) {
-                    return element;
+
+        if (page !== undefined || page !== null) {
+            await page.waitForNavigation({ timeout: 3000 }).catch(e => void e);
+            await page.waitFor(6000).catch(e => void e);
+            let elements = []
+
+            elements = await page.$x(xpath).catch(e => void e);
+
+            if (elements !== undefined && elements.length > 0) {
+                for (let element of elements) {
+                    let text = await (await element.getProperty('textContent')).jsonValue();
+                    const propertyHandleValue = await (await element.getProperty('value')).jsonValue();
+                    text = HtmlUtil.isUrl(text) ? text : TextUtil.normalizeText(TextUtil.removeWhiteSpace(text));
+                    text = text != undefined && text.length > 0 ? text :
+                        propertyHandleValue != undefined && propertyHandleValue.length > 0 ?
+                            TextUtil.normalizeText(TextUtil.removeWhiteSpace(propertyHandleValue)) : '';
+                    if (text === searchValue) {
+                        return element;
+                    }
                 }
             }
         }
@@ -143,7 +154,7 @@ export default class PuppeteerUtil {
     }
 
     static checkDuplicateNode(arrayNodes, text, currentNode, currentUrl, edgesList = null) {
-        
+
         if (HtmlUtil.isUrl(text)) {
             let urlsList = [];
             urlsList.push.apply(urlsList, TextUtil.getUrlsNodes(arrayNodes))
@@ -160,7 +171,7 @@ export default class PuppeteerUtil {
 
             text = (/\d{2,20}(\/)\d{4}/.test(text)) && !isDate ? text.substring(text.length - 4, text.length) : text;
             const currentValue = currentNode.getSource().getValue();
-        
+
             if (isDate) {
                 return true;
             }
@@ -173,7 +184,7 @@ export default class PuppeteerUtil {
                 let value = node.getSource().getValue();
                 value = (/\d{2,20}(\/)\d{4}/.test(value)) && !(/\d{2}(\/)\d{2}(\/)\d{4}/.test(value)) ? value.substring(value.length - 4, value.length) : value;
 
-                value = ( isnum && (/^\d+$/.test(value)))  ? value.substr(0, 2) : value;
+                value = (isnum && (/^\d+$/.test(value))) ? value.substr(0, 2) : value;
 
                 if (isnum && StringSimilarity.compareTwoStrings(text, value) > 0.95) {
                     return true;
