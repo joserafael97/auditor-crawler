19400ba5f81166004fdcdff519b700cd77f682e4
joserafael97 <jose.rafael.feitosa@gmail.com>
2019-11-27 23:32:20 +0000

    corretions

--- core/config/config.dev.js
+++ core/config/config.dev.js
@@ -10,14 +10,25 @@ config.dbHost = process.env.dbHost || 'localhost';
 config.dbPort = process.env.dbPort || '27017';
 
 
-const allItens = process.argv.slice(4)[0] !== undefined ? 
-CliParamUtil.allItensParamExtract(process.argv.slice(4)[0]) === "true" ? true : false: undefined;
+const allItens = process.argv.slice(4)[0] !== undefined && CliParamUtil.allItensParamExtract(process.argv.slice(4)[0]) === "true" ? true : 
+        process.argv.slice(2)[0] !== undefined && CliParamUtil.allItensParamExtract(process.argv.slice(2)[0]) === "true" ? true : false;
+
+console.log("allItens: ", allItens)
+
 if (allItens !== undefined && !allItens) {
+    // config.dbName = process.env.dbName || 'auditor-crawler-test-lessitens';
+    // config.dbName = process.env.dbName || 'auditor-crawler-exp02';
+
     config.dbName = process.env.dbName || 'auditor-crawler-test';
-    // config.dbName = process.env.dbName || 'auditor-crawler-exp02-notitens';
+
+
 } else {
-    // config.dbName = process.env.dbName || 'auditor-crawler-test';
+    // config.dbName = process.env.dbName || 'auditor-crawler-test-allitens';
+    // config.dbName = process.env.dbName || 'auditor-crawler-exp01';
     config.dbName = process.env.dbName || 'auditor-crawler-exp01-expandit';
+
+    // config.dbName = process.env.dbName || 'auditor-crawler-test';
+
 }
 
 config.serverPort = process.env.serverPort || 3000;
