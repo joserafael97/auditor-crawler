18fdde6f54423ea860c448b8501b72b9f52960b4
joserafael97 <jose.rafael.feitosa@gmail.com>
2019-12-04 22:04:59 +0000

    corretions run to Barra De Santa Rosa

--- runAllcounties.sh
+++ runAllcounties.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-aproach="bfs"
+aproach="dfs"
 
-cat counties.txt | xargs -P 7  -d '\n' -l1 -I value nohup npm start county=value aproach="$aproach" allitens="false" > runLogs/"$value"_"$aproach"_"$(date +%d_%m_%Y_%H_%M_%S_%N)".out.txt 2>&1 &
+cat counties.txt | xargs -P 6  -d '\n' -l1 -I value nohup npm start county=value aproach="$aproach" allitens="false" > runLogs/"$value"_"$aproach"_"$(date +%d_%m_%Y_%H_%M_%S_%N)".out.txt 2>&1 &
