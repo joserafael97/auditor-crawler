cc9986231a79f22e2b9863093492ca045bf90066
joserafael97 <jose.rafael.feitosa@gmail.com>
2019-12-10 17:53:11 +0000

    change files to run

--- runAllcounties.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-aproach="dfs"
-
-cat counties.txt | xargs -P 3  -d '\n' -l1 -I value nohup npm start county=value aproach="$aproach" allitens="true" > runLogs/"$value"_"$aproach"_"$(date +%d_%m_%Y_%H_%M_%S_%N)".out.txt 2>&1 &
